8a8e956b3b99f379f8cdc4ef0ba0e1f25e7dc870
kemal.ongun <kemal.ongun@aurorabilisim.com>
2023-02-27 13:45:17 +0300

    ecmascript 6 new features

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 
-const slugify = require("slugify");
+import hello, {collect, subs }  from "./my-module.js";
+import slugify from "slugify";
 
 const title = slugify("Kemal Abdullah Ongun", "-");
+
 console.log(title);
+hello("ongun");
+console.log(collect(1,1));
+console.log(subs(1,1));
